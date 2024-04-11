cd65d788fc10fd1b2a842e1a78f1f6fdfae68d09
D Rao <tdayakarrao@gmail.com>
2024-04-11 20:42:16 +0530

    Week-6: PA-4

--- NPTEL_Java_Jan-April_2024/src/Week_6_PA/W6_PA4.java
+++ NPTEL_Java_Jan-April_2024/src/Week_6_PA/W6_PA4.java
@@ -1,5 +1,38 @@
 package Week_6_PA;
 
-public class W6_PA4 {
+import java.util.Scanner;
+public class W6_PA4{     
+  
+    public static void main(String[] args) 
+    {
+        double sum = 0.0;
+        double standardDeviation = 0.0;
+        double mean = 0.0;
+        double res = 0.0;
+        double sq = 0.0;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        double arr[] = new double[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        // write code to find standard deviation of input array		
+    
+        for (double i : arr) {
+      
+        	sum += i;
+        }
+    
+        int length = arr.length;
+        mean = sum / length;    
+    
+        for (double number : arr){
+      
+        	standardDeviation += Math.pow(number - mean, 2);
+        }
 
+        res = Math.sqrt(standardDeviation / length);
+
+        System.out.print("Standard Deviation: " + res);
+    }
 }
