3c8dba26b484ac022e2bae53f00aad3402eff6ed
D Rao <tdayakarrao@gmail.com>
2024-04-12 01:48:10 +0530

    Week-7: PA-5

--- NPTEL_Java_Jan-April_2024/src/Week_7_PA/W7_PA5.java
+++ NPTEL_Java_Jan-April_2024/src/Week_7_PA/W7_PA5.java
@@ -1,2 +1,52 @@
 package Week_7_PA;
 
+import java.util.Scanner;
+
+interface Flyable {
+    void fly();
+}
+
+interface Swimmable {
+    void swim();
+}
+// TODO: Create a class "FlyingFish" that implements both "Flyable" and "Swimmable" interfaces.
+
+class FlyingFish implements Flyable, Swimmable{
+  
+  private String fishName;
+  
+  public FlyingFish(String fishName){
+    
+    this.fishName = fishName;
+    
+  }
+  
+  public void fly(){
+    
+    System.out.println( fishName + " can glide through the air");    
+    
+  }
+  
+  public void swim(){
+    
+    System.out.print( fishName + " can swim in water");    
+    
+  }  
+  
+}
+public class W7_PA5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // System.out.print("Enter the name of the fish: ");
+        String fishName = scanner.nextLine();
+
+        FlyingFish flyingFish = new FlyingFish(fishName);
+
+        // Test the FlyingFish class
+        flyingFish.fly();
+        flyingFish.swim();
+
+        scanner.close();
+    }
+}
