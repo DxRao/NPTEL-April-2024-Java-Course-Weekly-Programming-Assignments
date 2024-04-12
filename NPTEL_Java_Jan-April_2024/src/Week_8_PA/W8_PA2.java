2b4e75ad957df517ce1b38449e75b5208d18453f
D Rao <tdayakarrao@gmail.com>
2024-04-12 16:17:18 +0530

    Week-8: PA-2

--- NPTEL_Java_Jan-April_2024/src/Week_8_PA/W8_PA2.java
+++ NPTEL_Java_Jan-April_2024/src/Week_8_PA/W8_PA2.java
@@ -1,2 +1,24 @@
 package Week_8_PA;
 
+import java.util.Scanner;
+class W8_PA2
+{
+public static void main(String... a)
+    {        
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+//TODO code to generate harmonic series using while loops.
+        
+        double result = 0.0;
+        
+		while(num > 0)
+        {
+        	result = result + (double) 1 / num;
+           
+         	num--;          
+          
+          }
+        
+        System.out.print("Output of Harmonic Series is " + result);
+}
+}
