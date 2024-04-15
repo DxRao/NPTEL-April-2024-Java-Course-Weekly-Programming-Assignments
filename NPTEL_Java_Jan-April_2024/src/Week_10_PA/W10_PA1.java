985e724fe1bf3ad58b7f9ebd85bd64df01bbae1e
D Rao <tdayakarrao@gmail.com>
2024-04-15 07:13:09 +0530

    Week-10: PA-1

--- NPTEL_Java_Jan-April_2024/src/Week_10_PA/W10_PA1.java
+++ NPTEL_Java_Jan-April_2024/src/Week_10_PA/W10_PA1.java
@@ -1,2 +1,50 @@
 package Week_10_PA;
 
+import java.util.Scanner;
+
+public class W10_PA1
+{
+   public static void printArray(int[] array){
+    
+        // Iterating using for loops
+        for (int i = 0; i < array.length; i++) {
+            
+        	System.out.print(array[i] + " ");
+        }       
+    }
+   
+   //code to sort the elements of an array in ascending order.
+
+   public static void sortArray(int[] array){
+  
+	   for(int i = 1; i < array.length; i++){
+		   for(int j = 0; j < array.length; j++){
+      
+			   if(array[j] > array[i]){
+        
+				   int temp = array[j];
+				   array[j] = array[i];
+				   array[i] = temp;       
+        
+			   }        
+		   } 
+	   }    
+}
+public static void main(String... a){
+            
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] array = new int[size];
+        int i;
+        for (i = 0; i < size; i++) {
+            array[i] = s.nextInt();
+        }
+        
+        sortArray(array);
+        
+        // Displaying elements of array after sorting
+         System.out.println("Elements of array sorted in ascending order:");
+         
+         printArray(array);
+    }
+}
