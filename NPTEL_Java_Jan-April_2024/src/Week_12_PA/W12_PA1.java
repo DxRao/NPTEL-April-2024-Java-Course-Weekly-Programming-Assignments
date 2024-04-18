03adc1e52102132d6f4a4fefcff6276ce4fe5cf4
D Rao <tdayakarrao@gmail.com>
2024-04-19 00:05:02 +0530

    Week-12: PA-1

--- NPTEL_Java_Jan-April_2024/src/Week_12_PA/W12_PA1.java
+++ NPTEL_Java_Jan-April_2024/src/Week_12_PA/W12_PA1.java
@@ -1,2 +1,56 @@
 package Week_12_PA;
 
+import java.util.Scanner;
+
+public class W12_PA1 {
+// Create a static function calculateGrade() to calculate the grade as needed and 
+//return to main() where it will automatically get printed
+
+	static char calculateGrade(int score){
+  
+		if (score >= 80){
+    
+			return 'A'; 
+    
+		}
+		else if(score >= 70){
+    
+			return 'B';
+    
+		}
+		else if(score >= 60){
+    
+			return 'C';
+    
+		}
+		else if(score >= 50){
+    
+			return 'D';
+    
+		}
+		else if(score >= 40){
+    
+			return 'P';
+    
+		}
+		else{
+    
+			return 'F';
+    
+		}  
+  
+}
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter the numerical score: ");
+        int score = scanner.nextInt();
+        scanner.close();
+        // If score is greater than 100 or less than 0, print "Invalid Input"
+        if (score > 100 || score < 0) {
+            System.out.println("Invalid Input");
+            return;
+        }
+        char grade = calculateGrade(score);
+        System.out.println("Grade: " + grade);
+    }
+}
